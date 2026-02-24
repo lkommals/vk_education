5f17d49ef6017328b7de6d1c6b73240c46e8653a
root <root@WIN-85C61P9ETAJ.localdomain>
2026-02-24 08:56:03 +0300

    done all tasks

--- algo/hw-01/hw.py
+++ algo/hw-01/hw.py
@@ -22,7 +22,7 @@ def rotate_array(arr, k):
     reverse_by_indexes(arr, 0, k - 1) 
     reverse_by_indexes(arr, k, n - 1) 
 
-def merge_sorted(arr1, arr2): 
+def merge(arr1, arr2): 
     '''
     Дано два отсортированных по возрастанию массива. Необходимо написать функцию, которая объединит эти два массива в один отсортированный за линейное время. 
     '''
@@ -50,17 +50,91 @@ def merge_sorted(arr1, arr2):
         ind += 1
     return res 
 
-def merge_sorted_without_allocations(arr1, arr2): 
+def merge_without_allocations(arr1, arr2): 
     '''
-    Даны два отсортированных по возрастанию массива. Первый массив имеет размер результирующего массива. Функция объединяет эти массивы в первый, без дополнительных аллокаций за линейное время. 
+    Даны два отсортированных по возрастанию массива. Первый массив имеет размер результирующего массива, пустующие ячейки заполнены нулями. Функция объединяет эти массивы в первый, без дополнительных аллокаций за линейное время. 
     '''
-    
+    size_1 = len(arr1) 
+    size_2 = len(arr2) 
+    right = size_2 - 1
+    left = size_1 - size_2 - 1 
+    curr_ind = size_1 - 1
+
+    while curr_ind >= 0:
+        if left >= 0 and arr1[left] >= arr2[right]:
+            arr1[curr_ind] = arr1[left]
+            left -= 1
+        else:
+            arr1[curr_ind] = arr2[right] 
+            right -= 1 
+        curr_ind -= 1
+    return arr1 
+
+def zeros_ones_sort(arr):
+    '''
+    Дан массив, содержащий только 0 и 1. Отсортировать массив так, чтобы все нули оказались в начале, а все единицы - в конце. Решение должно быть in-place. 
+    '''
+    left = 0
+    right = len(arr) - 1
+    while left < right:
+        while arr[left] == 0:
+            left += 1
+        while arr[right] == 1:
+            right -= 1
+        if left < right: 
+            arr[left], arr[right] = arr[right], arr[left] 
+    return arr 
+
+def netherland_flags(arr):
+    '''
+    Дан массив, состоящие из 0, 1 и 2. Отсортировать его за линейное время
+    '''
+    n = len(arr) 
+    left = 0 
+    right = n - 1 
+    mid = left + 1
+    while mid < right:
+        while arr[left] == 0:
+            left += 1
+        while arr[mid] == 1:
+            mid += 1
+        while arr[right] == 2:
+            right -= 1
+        if left < mid and arr[left] == 1 and arr[mid] == 0:
+            arr[left], arr[mid] = arr[mid], arr[left] 
+        if mid < right and arr[mid] == 2 and arr[right] != 2:
+            arr[mid], arr[right] = arr[right], arr[mid] 
+        if left < right and arr[left] == 2 and arr[right] == 0:
+            arr[left], arr[right] = arr[right], arr[left]
+    return arr 
+
+def reverse_evens(arr):
+    '''
+    функция для переноса в начало неотсортированного массива четных чисел. Перераспределение стабильно.  
+    '''
+    n = len(arr) 
+    index = 0 
+    for i in range(n):
+        if arr[i] % 2 == 0:
+            arr[index], arr[i] = arr[i], arr[index] 
+            index += 1
+    return arr 
+
+def zeros_last(arr):
+    '''
+    Перенос ненулевых элемнтов в начало массива за один проход и стабильно
+    '''
+    n = len(arr)
+    index = 0
+    for i in range(n):
+        if arr[i] != 0:
+            arr[index], arr[i] = arr[i], arr[index]
+            index += 1
+    return arr
 
 def main():
-    arr1 = [1,2,3, 7,]
-    arr2 = [1]
-    res = merge_sorted(arr1, arr2) 
-    print(res) 
+    arr = [4, 0, 0, 6, 1, 8, 1, 10, 0] 
+    print(zeros_last(arr)) 
 
 if __name__ == '__main__':
     main() 
