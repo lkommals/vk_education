76c1a57abfca4ae5e151f54474f16f748857313c
root <root@WIN-85C61P9ETAJ.localdomain>
2026-03-01 10:42:28 +0300

    middle and remove element functions implemented

--- algo/hw-02/hw.py
+++ algo/hw-02/hw.py
@@ -21,4 +21,35 @@ def reverse_node(head):
         prev = curr 
         curr = tmp  
     return prev 
-        
+
+def middle(head):
+    '''
+    Найти середину списка за O(n) без дополнительных аллокаций. Использовать метод двух указателей
+    '''
+    slow = head 
+    fast = head 
+    while fast and fast.next:
+        fast = fast.next.next 
+        slow = slow.next 
+    return slow 
+
+def removeElem(head, val):
+    '''
+    Удалить элемент ноду со значением val из списка head. Удаление происходит без создания нового списка. 
+    '''
+    if head.val == val: 
+        res = head.next 
+        head.next = None
+        return res  
+    else: 
+        curr = head 
+        prev = curr 
+        while curr != None and curr.val != val:
+            prev = curr 
+            curr = curr.next 
+        if curr != None: 
+            prev.next = curr.next 
+            curr.next = curr 
+        return head 
+
+
