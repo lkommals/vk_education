f551e0d1692044cb973e14d19479f4fdbb45bda0
root <root@WIN-85C61P9ETAJ.localdomain>
2026-03-09 09:00:57 +0300

    added history deque for error handling

--- aggregator/aggregator.py
+++ aggregator/aggregator.py
@@ -1,9 +1,19 @@
 import functools 
+from collections import deque 
 
-def circuit_breaker(state_count, error_count, networks_error, sleep_time_sec):
+def circuit_breaker(state_count, error_count, network_errors, sleep_time_sec):
+    history = deque(maxlen=state_count) 
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            return func(args, kwargs) 
+            nonlocal history 
+            try:
+                result = func(*args, **kwargs)  
+            except network_errors:
+                history.append(False) 
+                raise 
+            else:
+                history.append(True) 
+                return result 
         return wrapper 
     return decorator
