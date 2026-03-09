27140856cd4709de2b1479a67ca5f6ef8f1027e2
root <root@WIN-85C61P9ETAJ.localdomain>
2026-03-09 09:31:11 +0300

    added check if too many errors occured

--- aggregator/aggregator.py
+++ aggregator/aggregator.py
@@ -1,12 +1,18 @@
 import functools 
 from collections import deque 
 
+class NotAliveError(Exception): 
+    pass 
+
 def circuit_breaker(state_count, error_count, network_errors, sleep_time_sec):
     history = deque(maxlen=state_count) 
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             nonlocal history 
+            if len(history) >= error_count: 
+                if all(entry is False for entry in list(history)[-error_count:]):
+                    raise NotAliveError("Too many errors!")
             try:
                 result = func(*args, **kwargs)  
             except network_errors:
